b19673953ce6d81dcf05413b029074e326223e74
Henwee <heenwee@gmail.com>
2022-04-05 15:25:16 +0200

    map creation (maybe?)

--- TFB/TFB.ino
+++ TFB/TFB.ino
@@ -1,13 +1,35 @@
-int gridSizeX, gridSizeY;
-
+#define gridSizeX 20
+#define gridSizeY 20
+Tile tiles [gridSizeX][gridSizeY];
 
 void setup() {
   Serial.begin(9600); // begin transmission
+  SetupMap();
 }
 void loop() {
   Communicate();
 }
 
+void SetupMap()
+{
+  for (int x = 0; x < gridSizeX; x++){
+    for (int y = 0; y < gridSizeY; y++){
+      if((x == 0 || x == gridSizeX) || (y == 0)) tiles[x][y].isSolid = true;
+    }
+  }
+  PrintMap();
+}
+
+void PrintMap() {
+  for (int x = 0; x < gridSizeX; x++){
+    Serial.println();
+    for (int y = 0; y < gridSizeY; y++){
+      Tile t = tiles[x][y];
+      if(t.isSolid) Serial.print("#");
+      else          Serial.print(".");
+    }
+  }
+}
 void Communicate() {
   String val;
   while (Serial.available() > 0) {
@@ -21,5 +43,5 @@ void Communicate() {
 
 class Tile {
   public:
-    int x, y;
+    bool isSolid = false;
 };
