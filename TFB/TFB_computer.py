59b5dad359aaa2281237502728fda7ecec1caf8e
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-04-05 17:47:31 +0200

    tbf :)

--- TFB/TFB_computer.py
+++ TFB/TFB_computer.py
@@ -1,11 +1,23 @@
 import serial
 from serial import Serial
-ser = serial.Serial("/dev/ttyACM0")  # open serial port
-print(ser.name)         # check which port was really used
-stringy = input("Enter a string: ")
-print(stringy)
 
-try:
-    ser.write(stringy.encode())
-except:
-    ser.close()             # close port
+if __name__ == '__main__':
+    ser = serial.Serial("/dev/ttyACM0")  # open serial port
+    print(ser.name) # double check to make sure that the code is using the correct port or not
+
+    while True:
+        data = input("Enter a string: ")
+        print(data)
+
+        try:
+            ser.write(data.encode())
+        except:
+            ser.close() # close port
+
+
+def main():
+    pass
+
+
+def print_board(input):
+    print(input)
