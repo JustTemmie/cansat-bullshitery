81991bf8a90e142e19179c8d725c6ce16279dca0
Henwee <heenwee@gmail.com>
2022-04-06 12:25:14 +0200

    playable (kinda)

--- TFB/TFB.ino
+++ TFB/TFB.ino
@@ -8,10 +8,19 @@ class Tile {
 Tile tiles [gridSizeX][gridSizeY];
 
 int lLineCollum, rLineCollum;
-#define lineFromWall 3
+#define lineFromWall 4
 #define lineWidth 5
 float ballX, ballY;
 int tileBallX, tileBallY;
+
+float angle = 45;
+float speed = 1;
+float xVel, yVel;
+#define delayTime 150
+
+int lLineTiles [lineWidth];
+int rLineTiles [lineWidth];
+int computerLineMove;
 
 void setup() {
   Serial.begin(9600); // begin transmission
@@ -19,6 +28,10 @@ void setup() {
   pinMode(6, OUTPUT);
 }
 void loop() {
+  delay(delayTime);
+  MoveLines();
+  MoveBall();
+  PrintMap();
   Communicate();
 }
 
@@ -41,10 +54,14 @@ void InstertLines()
 
   int distFromEdge = (gridSizeY-lineWidth)/2; // finds how far the lines will be from the edges
 
+  int i = 0;
   for (int y = 0; y < gridSizeY; y++){ // loop through the y axis
     if(y > distFromEdge-1 && y < gridSizeY-distFromEdge){
-      tiles[lLineCollum][y].isSolid = true;
+      //tiles[lLineCollum][y].isSolid = true;
+      lLineTiles[i] = y;
       tiles[rLineCollum][y].isSolid = true;
+      rLineTiles[i] = y;
+      i++;
     }
   }
 }
@@ -52,9 +69,11 @@ void SetupBall(){
   // set the ball to the middle of the screen
   ballX = gridSizeX/2;
   ballY = gridSizeY/2;
-  
-  tileBallX = (int)(ballX+0.5f);
-  tileBallY = (int)(ballY+0.5f);
+  //set the velocity of the ball
+  xVel = cos(angle);
+  yVel = sin(angle);
+
+  MoveLines();
   MoveBall();
 }
 
@@ -72,9 +91,38 @@ void PrintMap() {
 
 void MoveBall(){
   tiles[tileBallX][tileBallY].isSolid = false;
-  // code
+  
+  //find the next position to check if there's a tile in the way
+  int nextXPos = (int)(ballX+xVel+0.5f);
+  if(tiles[nextXPos][tileBallY].isSolid) {
+    xVel *= -1;
+  }
+  int nextYPos = (int)(ballY+yVel+0.5f);
+  if(tiles[nextYPos][tileBallX].isSolid) {
+    yVel *= -1;
+  }
+
+  ballX += xVel;
+  ballY += yVel;
+  
+  tileBallX = (int)(ballX+0.5f);
+  tileBallY = (int)(ballY+0.5f);
+
   tiles[tileBallX][tileBallY].isSolid = true;
 }
+
+void MoveLines()
+{
+  for (int i = 0; i < lineWidth; i++){
+    tiles[lLineCollum][lLineTiles[i]].isSolid = false;
+    if((lLineTiles[0] == 1 && computerLineMove == 1) ||
+        lLineTiles[lineWidth-1] == gridSizeY-2 && computerLineMove == -1) lLineTiles[i]-=computerLineMove;
+        
+    tiles[lLineCollum][lLineTiles[i]].isSolid = true;
+  }
+  computerLineMove = 0;
+}
+
 void Communicate() {
   String val;
   while (Serial.available() > 0) {
@@ -82,9 +130,7 @@ void Communicate() {
   }
   float n = val.toFloat(); // converts the input to a number (returns 0 if input was a string)
   if (n != 0) {
-    Serial.println(n);
-    if(n == 6.0f){
-      digitalWrite(6, HIGH);
-    }
+    computerLineMove = (int)n;
+    Serial.print(computerLineMove);
   }
 }
