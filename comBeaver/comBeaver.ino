cf8aa1cc2a0a36d993acaf3df385e75a82387b5b
Henwee <heenwee@gmail.com>
2022-04-12 17:05:54 +0200

    returns string

--- comBeaver/comBeaver.ino
+++ comBeaver/comBeaver.ino
@@ -8,8 +8,9 @@ void loop(){
   while (Serial.available() > 0) {
     val = val + (char)Serial.read(); // read data byte by byte and store it
   }
-  float n = val.toFloat(); // converts the input to a number (returns 0 if input was a string)
+  Serial.println(val)
+;  /*float n = val.toFloat(); // converts the input to a number (returns 0 if input was a string)
   if (n != 0) {
     Serial.println(100000+n);
-  }
+  }*/
 }
