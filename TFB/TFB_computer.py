6b90276f7e204f205bc18aed1053836b4278318e
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-04-18 16:35:35 +0200

    bdzvf

--- TFB/TFB_computer.py
+++ TFB/TFB_computer.py
@@ -14,28 +14,23 @@ if __name__ == '__main__':
     n = 0
     funny = "1"
     
-    ser = serial.Serial("/dev/ttyUSB0", 9600)
+    input = serial.Serial("/dev/ttyUSB1", 9600)
+    output = serial.Serial("/dev/ttyACM0", 9600)
     time.sleep(0.4)
-    print(ser.name)
-    
-    ser.flushInput()
+    print(input.name)
+    print(output.name)
+        
+    input.flushInput()
+    output.flushOutput
 
     while True:
         time.sleep(0.2)
-        ser_bytes = ser.readline()
-        decoded_bytes = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-        print(decoded_bytes)
+        ser_bytes = input.readline()
+        #decoded_bytes = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+        print(ser_bytes)
         
         #n += 1
         #time.sleep(0.05)
         
-        ser.write(funny.encode())
+        #output.write(funny.encode())
         #print("sent " + str(funny))
-        #n = 0
-        
-        
-        #try:   
-        #    ser.write(data.encode()) # encode is a function that takes in a string and returns a byte string that you can use to send data to the arduino
-        #except:
-        #    ser.close() 
-        #    print("Error") 
