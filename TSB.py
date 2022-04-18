f3e91b25e69db688f5853c5c67a0a846ea4da019
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-04-18 14:10:34 +0200

    comments, actually not auto generated lmao

--- TSB.py
+++ TSB.py
@@ -5,60 +5,91 @@ import time
 import random
 
 print_axis = True
-debug_info = False
-chonk_mode = True
+debug_info = True
+chonk_mode = False #chonk mode is made mostly as a joke, it just doubles the side of everything in both dimensions
+
+device = "/dev/ttyACM0"
+ser_rate = 9600
+
+ball = "😳"
+skip_ball_offset = True # some balls like the 😳 emoji for example, are actually twice the width of a normal character, if the ball you're using is two wide turn this to True
+topbottom = "--"
+sides = "|"
+corner = "x"
+paddles = "I"
 
 rows = 15
 cols = 15
+paddle_chonk = 5 # having a paddle chonk of 2 would mean the paddle is 5 tall, the main point with two over and two under
 
 score_TFB = 0
 score_TSB = 0
 
+#declare a 2D array, this is used instead of a normal 1D array as the game is 2D and it's waaay easier
 arr = [[" " for i in range(rows)] for j in range(cols)]
 
 def main(): 
     pass 
 
+
+#the write function only exists because the code "arr[2][6]" would write to x 6 and y 2, this function just flips them around
 def write(x, y, character):
     arr[round(y)][round(x)] = character
 
 
 def clear_board():
+    #loops thru the array and sets up the board
     for y in range(0, rows):
         for x in range(0, cols):
+            
+            #checks where the array index is on the board and writes to it accordingly
             if (y == 0 or y == rows - 1) and (x == 0 or x == cols - 1):
-                write(x, y, "x")
+                write(x, y, corner)
             elif y == 0 or y == rows - 1:
-                write(x, y, "--")
+                write(x, y, topbottom)
             elif x == 0 or x == cols - 1:
-                write(x, y, "|")
+                write(x, y, sides)
             else:
                 write(x, y, " ")
 
 
+def write_ball(x, y):
+    write(x, y, ball)
+
 def write_paddles(TFB, TSB):
+    #write the paddles to the board using the balls Y coordinate
     for y in range(0, rows):
         if y == TFB:
-            for u in range(y - 2, y + 3):
+            for u in range(y - paddle_chonk, y + paddle_chonk+1):
                 if u != 0 and u != rows - 1:
-                    write(3, u, "I")
+                    write(3, u, paddles)
             
         if y == TSB:
-            for u in range(y - 2, y + 3):
+            for u in range(y - paddle_chonk, y + paddle_chonk+1):
                 if u != 0 and u != rows - 1:
-                    write(11, u, "I")
+                    write(cols-4, u, paddles)
             
 
 def add_visual_data(row, a):
     for i in arr[a]:
         row += str(i)
-        if chonk_mode: row += str(i)
-        if i != "x" and i != "--" and i != "|": 
-            row += " "
-            if chonk_mode: row += " "
+        if chonk_mode: row += str(i) # doubles the width if chonk mode is on
+        
+        #adds a space to the end of I, such that the board is almost rectangular, as opposed to a tall and slim square
+        if i != corner and i != sides and i != topbottom:
+            if skip_ball_offset:
+                if i != ball:
+                    row += " "
+                    if chonk_mode: row += " "
+            
+            else:
+                row += " "
+                if chonk_mode: row += " "
+            
     return row
     
 def print_game():
+    #loops thru every row and prints the content to the terminal
     for a in range(0, rows):
         row = ""
         row += add_visual_data(row, a)
@@ -71,6 +102,7 @@ def print_game():
             if print_axis: print(row, a)
             else: print(row)
 
+    #displays the axis after the board is finished printing
     if print_axis:
         if chonk_mode:
             print("0   1  2   3  4   5   6   7   8   9   10  11  12  13  14")
@@ -83,14 +115,17 @@ def print_game():
 if __name__ == '__main__': 
     clear_board()
 
+    #declare variables that will be used in the future
     n = 0
-    pos = 7
+    pos = int((rows-1)/2)
     funny = "1"
     
-    ser = serial.Serial("/dev/ttyACM0", 9600)
+    #initialize serial
+    ser = serial.Serial(device, ser_rate)
     time.sleep(0.4)
     print(ser.name)
     
+    #flush the serial port to clear any garbage
     ser.flushInput()
     
 
@@ -99,22 +134,29 @@ if __name__ == '__main__':
         clear_board()
         time.sleep(0.1)
         ser_bytes = ser.readline()
+        #decodes the input into a string and removes some garbage
         decoded_bytes = (ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+        
+        #splits the input at every point where it sees a comma
+        #this is done so that the script can very easily use the incoming data
+        #for example i can just ask what array[1] is and it will return the X position for the ball
         array = decoded_bytes.split(",")
         
         
     
         try:
-            write(float(array[1]), float(array[2]), "O") # ball x and y
+            write_ball(float(array[1]), float(array[2]))
             write_paddles(round(float(array[4])), round(float(array[5]))) # TFB and TSB
         except:
             pass
             #print("error in trying to update the board")
         
         try:
-            if round(float(array[2])) < pos and pos > 3: # if y is less than the paddle position
+            #checks where the ball is relative to the player's paddle
+            #also double checks that it's not moving the paddle out of bounds
+            if round(float(array[2])) < pos and pos > paddle_chonk+1:
                 pos -= 1
-            elif round(float(array[2])) > pos and pos < rows - 4: # if y is greater than the paddle position
+            elif round(float(array[2])) > pos and pos < rows - paddle_chonk - 2:
                 pos += 1
             else:
                 pos += 0
@@ -131,12 +173,9 @@ if __name__ == '__main__':
         except:
             pass
 
-       
-        #if pos < 13:
-        #    pos += 1
-        #else:
-        #    pos = 2
+
         
+        #prints debuf info meaning the array, and the position of the paddle
         if debug_info:
             print(array)
             try:
@@ -149,17 +188,10 @@ if __name__ == '__main__':
         try:
             ser.write(str(pos).encode())
         except:
-            print("failed to write to TFB")
+            print("failed whilst writing to TFB")
         
-        #time.sleep(0.5)
+
+
+        #prints the score and the game board
         print(f"TFB score = {score_TFB} and TSB score = {score_TSB}")
         print_game()
-        #print("sent " + str(funny))
-        #n = 0
-        
-        
-        #try:   
-        #    ser.write(data.encode()) # encode is a function that takes in a string and returns a byte string that you can use to send data to the arduino
-        #except:
-        #    ser.close() 
-        #    print("Error") 
