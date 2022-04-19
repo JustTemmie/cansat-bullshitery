8b815316d290a0fb970a409683e169bc7abf4640
Henwee <heenwee@gmail.com>
2022-04-19 11:33:39 +0200

    i fix (:

--- TFB/TFB.ino
+++ TFB/TFB.ino
@@ -245,7 +245,7 @@ void MoveBall(){
   
   // check if ball hits the right line
   if((int)(nextXPos+0.5f) == rLineCollum){
-    if(nextYPos <= desiredAiPos+lineChonk && nextYPos >= desiredAiPos-lineChonk){
+    if(nextYPos <= rLinePos+lineChonk && nextYPos >= rLinePos-lineChonk){
       xVel *= -1; // flip
     }
   }
@@ -278,7 +278,7 @@ void MoveLines()
 
   #pragma region aiLine
   desiredAiPos = tileBallY;
-  if(desiredAiPos < lineWidth-1-lineChonk && desiredAiPos > lineChonk)
+  if(desiredAiPos <= lineWidth-1-lineChonk && desiredAiPos >= lineChonk)
   {
     for (int j = 0; j < lineWidth; j++){
       tiles[rLineCollum][rLineTiles[j]].isSolid = false; // disable the tiles for the line temporarily
